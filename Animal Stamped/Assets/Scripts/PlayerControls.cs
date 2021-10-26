f68ad85928c3fb4dd9ac08c9812a39803c45c27c
Mathew Harris <10959101@uvu.edu>
2021-10-26 13:00:01 -0600

    food Prefab
    Created food prefab and attached it to player object

--- Animal Stamped/Assets/Scripts/PlayerControls.cs
+++ Animal Stamped/Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,8 @@ public class PlayerControls : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject foodPrefab;
+
 
     // Start is called before the first frame update
     void Start()
